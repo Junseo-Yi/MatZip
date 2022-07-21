4eafa9d6c3ecd998952d7bed912d59436c295c69
Junseo_Yi <mfgim1726@gmail.com>
2022-07-21 21:47:50 +0900

    make marker and for loop

--- index_script.js
+++ index_script.js
@@ -20,3 +20,34 @@ for (let i=0; i<cat_array.length; i++){
       document.getElementById(cat_array[i].name)
   );
 }
+//지도 만드는 부분
+var gagelist = [{lat:35.14179406784828, lng:129.1093778542578},{lat:35.14222540070751, lng:129.10905986268358}];
+
+
+function myMap(){
+  var ggumteo = {lat: 35.14247873924202, lng: 129.1087427604829};
+  var mapOptions = {
+    center:{lat:35.1411406069967, lng: 129.11008989467717},
+    zoom:17
+  };
+
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions );
+  var marker_ggumteo = new google.maps.Marker({position: ggumteo, map: map}); //마커 하나 만들기
+  for (var i=0; i<gagelist.length; i++){
+    var markers = new google.maps.Marker({
+      position: gagelist[i],
+      map: map
+    });
+}
+
+}
+
+//마커 여러 개 만들기
+/*function drawMarkers(gagelist){
+  for (var i = 0; i < gagelist.length; i++) {
+		var marker = new google.maps.Marker({
+			map: map,
+			position: new google.maps.LatLng(totalItems[i].la_crd, totalItems[i].lo_crd)
+		});
+}
+//new google.maps.LatLng(gagelist[i].lat, gagelist[i].lng)*/
