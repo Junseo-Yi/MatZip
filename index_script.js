dce0bb47cd21f87b0343db806fd753dc31645bdc
Junseo_Yi <mfgim1726@gmail.com>
2022-07-23 19:00:46 +0900

    신전, 타코 추가

--- index_script.js
+++ index_script.js
@@ -1,13 +1,29 @@
 
 //지도 만드는 부분
 var gage_loc = [{lat:35.14179406784828, lng:129.1093778542578},{lat:35.14222540070751, lng:129.10905986268358}];
+var gage_name = ['타코들며 쎄쎄쎄', '신전떡볶이'];
+var gage_score = [5,4];
+var gage_rec_menu = ['2인 세트', '치즈떡볶이'];
+var gage_more = ['https://m.place.naver.com/restaurant/1145800717/home?entry=plt',
+"https://m.place.naver.com/restaurant/75382121/home?entry=plt"]
 
+let infowindow_contents = [];
 
-let infowindow_contents = ['<div>이것은 시험용입니다</div>', '<div>시험용이라고요</div>'];
-let info_cnt = 0;
-/*function makeInfoWindow(place){
-  infowindow_contents[info_cnt] = place.name;
-}*/
+for (let i=0; i<gage_loc.length; i++){
+  let stars = '';
+  for (let j=0; j<gage_score[i]; j++){
+    stars += "★";
+  }
+  infowindow_contents[i] = '<div id="content">' +
+    '<h3 id="firstHeading" class="firstHeading">'+gage_name[i]+'</h3>' +
+    '<div id="bodyContent">' + '추천: ' + stars +
+    '<div style="color:green;">가게 추천 메뉴: <div style="color:black;display:inline;">' + gage_rec_menu[i] +'</div></div>'+
+    '추가정보:'+
+    `<a href= ${gage_more[i]}>` +
+    gage_more[i]+"</a>"+
+    "</div>" +
+    "</div>";
+}
 
 function myMap(){
   var ggumteo = {lat: 35.14247873924202, lng: 129.1087427604829};
@@ -19,6 +35,7 @@ function myMap(){
   var map = new google.maps.Map(document.getElementById("map"), mapOptions );
   var marker_ggumteo = new google.maps.Marker({position: ggumteo, map: map});
   const infowindow = new google.maps.InfoWindow();
+
   function create_marker_info(i){
     var marker = new google.maps.Marker({position: gage_loc[i], map: map});//위치별 마커찍기
     marker.addListener("click", () => {
@@ -33,10 +50,7 @@ function myMap(){
   for (let i=0; i<gage_loc.length;i++){
     create_marker_info(i);
   }
+
 }
-//infowindow 만들기
 
-
-
-//window 객체생성
 window.initMap = myMap;
